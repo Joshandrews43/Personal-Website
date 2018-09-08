ad36bd78d5d0318b2c9e24d63590d56674f9f43e
Joshandrews43 <joshandrews43@hotmail.com>
2018-09-08 15:21:14 -0700

    added heroku requirement

---
+++ index.js
@@ -0,0 +1,22 @@
+//index.js
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+const path = require('path')
+
+app.use('/static', express.static(__dirname + "/public"));
+
+app.use('/', router);
+
+app.set('views', __dirname + '/html');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Use the environment variable or use a given port
+const PORT = process.env.PORT || 8080;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:%s', PORT);
+});
